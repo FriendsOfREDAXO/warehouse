99fa9f2a6b31e15245c724c820a00108cfd8b723
Joachim Dörr <git@joachim-doerr.com>
2025-01-20 01:05:46 +0100

    block navigation and add css, js vendors

--- boot.php
+++ boot.php
@@ -1,15 +1,41 @@
 <?php
 
-rex_yform_manager_dataset::setModelClass('rex_warehouse_articles', FriendsOfRedaxo\Warehouse\Article::class);
+rex_yform_manager_dataset::setModelClass('rex_warehouse_articles', \FriendsOfRedaxo\Warehouse\Article::class);
 rex_yform_manager_dataset::setModelClass('rex_warehouse_categories', \FriendsOfRedaxo\Warehouse\Category::class);
 rex_yform_manager_dataset::setModelClass('rex_warehouse_orders', \FriendsOfRedaxo\Warehouse\Order::class);
 
 rex_yform::addTemplatePath($this->getPath('ytemplates'));
-// rex_yform::addTemplatePath(rex_path::addon('warehouse', 'ytemplates'));
-
 
 if (rex::isBackend()) {
+    rex_view::addJsFile($this->getAssetsUrl('scripts/wh-multiselect/wh_be_multiselect.js'));
+    rex_view::addJsFile($this->getAssetsUrl('scripts/autoNumeric.min.js'));
     rex_view::addJsFile($this->getAssetsUrl('scripts/be_script.js'));
+    rex_view::addCssFile($this->getAssetsUrl('styles/wh_be_multiselect.css'));
+    rex_view::addCssFile($this->getAssetsUrl('styles/backend.css'));
+
+    rex_extension::register('PACKAGES_INCLUDED', static function ($params) {
+        $plugin = rex_plugin::get('yform', 'manager');
+
+        if ($plugin->isAvailable()) {
+            $pages = $plugin->getProperty('pages');
+            $ycom_tables = [
+                'rex_warehouse_articles',
+                'rex_warehouse_categories',
+                'rex_warehouse_orders',
+                'rex_warehouse_article_variant',
+                'rex_warehouse_attributes',
+                'rex_warehouse_attributegroups'
+            ];
+
+            if (isset($pages) && is_array($pages)) {
+                foreach ($pages as $page) {
+                    if (in_array($page->getKey(), $ycom_tables)) {
+                        $page->setBlock('warehouse');
+                    }
+                }
+            }
+        }
+    });
 }
 
 if (rex::isFrontend()) {
@@ -18,13 +44,13 @@ if (rex::isFrontend()) {
 
     rex_extension::register('PACKAGES_INCLUDED', function () {
 
-        $user_path = rex_session('user_path','array');
+        $user_path = rex_session('user_path', 'array');
         if (!$_REQUEST || (count($_REQUEST) == 1) && isset($_REQUEST['PHPSESSID'])) {
             // ausschliessen: Fehler Artikel, Shop Detailseite
-            if (false === in_array(rex_article::getCurrentId(),[rex_article::getNotfoundArticleId(),25])) {
+            if (false === in_array(rex_article::getCurrentId(), [rex_article::getNotfoundArticleId(), 25])) {
                 $user_path[] = rex_article::getCurrentId();
-                $user_path = array_slice($user_path,-5);
-                rex_set_session('user_path',$user_path);
+                $user_path = array_slice($user_path, -5);
+                rex_set_session('user_path', $user_path);
 //                dump($user_path);
             }
         }
@@ -52,9 +78,9 @@ if (rex::isFrontend()) {
             $profile = $manager->getProfile();
             $seo = $manager->getSeo();
 
-            $data_id = (int) $manager->getDatasetId();
+            $data_id = (int)$manager->getDatasetId();
             if ($profile->getTableName() == rex::getTable('warehouse_articles')) {
-
+                // Artikel
                 if ($var_id = rex_get('var_id', 'int')) {
                     $article = FriendsOfRedaxo\Warehouse\Article::get_articles(0, [$data_id, $var_id], true);
                 } else {
@@ -64,7 +90,7 @@ if (rex::isFrontend()) {
                 $warehouse_prop['seo_title'] = $article->get_name();
                 $warehouse_prop['path'] = FriendsOfRedaxo\Warehouse\Warehouse::get_path($article->category_id);
             } elseif ($profile->getTableName() == rex::getTable('warehouse_categories')) {
-
+                // Kategorie
                 $warehouse_prop['sitemode'] = 'category';
                 $warehouse_prop['seo_title'] = $seo['title'];
                 $warehouse_prop['path'] = FriendsOfRedaxo\Warehouse\Warehouse::get_path($data_id);
@@ -92,12 +118,12 @@ if (rex::isFrontend()) {
                 if (rex_get('key') == $user_data['payment_confirm']) {
                     // Confirm Order in db
                     $order = \FriendsOfRedaxo\Warehouse\Order::query()
-                        ->where('payment_confirm',rex_get('key'))->where('payed',0)
+                        ->where('payment_confirm', rex_get('key'))->where('payed', 0)
                         ->findOne();
                     if (!$order) {
-                        rex_redirect(rex_config::get("warehouse","payment_error"));
+                        rex_redirect(rex_config::get("warehouse", "payment_error"));
                     }
-                    $order->setValue('payed',1);
+                    $order->setValue('payed', 1);
                     if (!$order->save()) {
                         rex_redirect(FriendsOfRedaxo\Warehouse\Warehouse::get_config("payment_error"));
                     }
