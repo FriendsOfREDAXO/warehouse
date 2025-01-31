470146a9a2d3d1bb0aa8f5cb4c9c80b383e67abb
Alexander Walther <mail@alexplus.de>
2025-01-31 22:31:24 +0100

    Tableset-Aktualisierung (Countries, Shipping)
    Umbenennung in `warehouse_country` und `warehouse_shipping` und Aktualisierungsprozess mit in `install.php` hinzugefügt.

--- install.php
+++ install.php
@@ -2,9 +2,11 @@
 
 $addon = rex_addon::get('warehouse');
 if (rex_addon::get('yform')->isAvailable()) {
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_category.json'));
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article.json'));
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article_variant.json'));
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_order.json'));
-  rex_yform_manager_table::deleteCache();
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article_variant.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_category.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_order.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_shipping.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_country.json'));
+    rex_yform_manager_table::deleteCache();
 }
