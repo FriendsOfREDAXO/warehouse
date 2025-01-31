f85e3b2dc69df8d3c613d606c45a1f7a30bcc784
Alexander Walther <mail@alexplus.de>
2025-01-31 22:27:11 +0100

    Im Installationsprozess URL-Profile installieren

--- install.php
+++ install.php
@@ -1,10 +1,13 @@
 <?php
 
+$this->includeFile(__DIR__ . '/install/update_scheme.php');
+$this->includeFile(__DIR__ . '/install/url_profile.php');
+
 $addon = rex_addon::get('warehouse');
 if (rex_addon::get('yform')->isAvailable()) {
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_category.json'));
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article.json'));
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article_variant.json'));
-  rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_order.json'));
-  rex_yform_manager_table::deleteCache();
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_category.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_article_variant.json'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/tablesets/warehouse_order.json'));
+    rex_yform_manager_table::deleteCache();
 }
