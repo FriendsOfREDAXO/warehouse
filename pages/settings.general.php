46e0204b6f4dbd016f79886a781799c0b65d3ba7
Alexander Walther <mail@alexplus.de>
2025-03-23 12:39:25 +0100

    Update settings.general.php

--- pages/settings.general.php
+++ pages/settings.general.php
@@ -1,57 +1,62 @@
 <?php
 
+use FriendsOfRedaxo\Warehouse\PayPal;
+
 $addon = rex_addon::get('warehouse');
 echo rex_view::title($addon->i18n('warehouse.title'));
 
 $form = rex_config_form::factory('warehouse');
 
-$form->addFieldset('Warehouse - Einstellungen');
-
-$field = $form->addTextField('store_name');
-$field->setLabel('Name des Shops');
-$field->setNotice('<code>rex_config::get("warehouse","store_name")</code> (z.B. <code>Martin Muster GmbH</code>) - wird z.B. bei PayPal Zahlung an PayPal übermittelt.');
-
-$field = $form->addTextField('store_country_code');
-$field->setLabel('Ländercode des Shops');
-$field->setNotice('<code>rex_config::get("warehouse","store_country_code")</code> (z.B. <code>de-DE</code>) - wird z.B. bei PayPal Zahlung an PayPal übermittelt.');
-
-$field = $form->addTextField('currency');
-$field->setLabel('Währung (z.B. EUR)');
-$field->setNotice('<code>rex_config::get("warehouse","currency")</code>');
+$field = $form->addSelectField('currency');
+$field->setLabel('Währung');
+$select = $field->getSelect();
+$select->addOptions(
+    PayPal::CURRENCY_CODES
+);
 
 $field = $form->addTextField('currency_symbol');
 $field->setLabel('Währungssymbol (z.B. €)');
 $field->setNotice('<code>rex_config::get("warehouse","currency_symbol")</code>');
 
-$field = $form->addTextField('country_codes');
-$field->setLabel('Mögliche Länder für die Lieferung');
-$field->setNotice('Als JSON in der Form <code>{"Deutschland":"DE","Österreich":"AT","Schweiz":"CH"}</code> angeben.<br><code>rex_config::get("warehouse","country_codes")</code>');
+$field = $form->addSelectField('shipping_allowed');
+$field->setLabel('Erlaubte Länder für die Lieferung');
+$select = $field->getSelect();
+$select->addOptions(
+    PayPal::COUNTRY_CODES
+);
+$field->setAttribute('multiple', 'multiple');
+$field->setAttribute('size', '10');
+
+
+// TODO: Warenkorb-Aktion ausblenden / einblenden in Formularen und in Optionen berücksichtigen / nicht berücksichtigen
 
 $field = $form->addSelectField('cart_mode');
-$field->setLabel('Warenkorb Modus');
+$field->setLabel('Nach Warenkorb-Aktion');
 $select = $field->getSelect();
 $select->addOptions([
-    'cart'=>'Warenkorb',
-    'page'=>'Artikelseite'
+    'cart'=>'in den Warenkorb wechseln',
+    'page'=>'auf der Artikelseite bleiben'
 ]);
+$select->setAttribute('disabled', 'disabled');
 
-$field->setNotice('Es kann entweder die Warenkorbseite aufgerufen werden oder die vorherige Artikelseite. Wenn die Artikelseite aufgerufen wird, so wird showcart=1 als Get-Parameter angehängt.<br><code>rex_config::get("warehouse","cart_mode")</code>');
+$field->setNotice('Es kann entweder die Warenkorbseite aufgerufen werden oder die vorherige Artikelseite. Wenn die Artikelseite aufgerufen wird, so wird showcart=1 als Get-Parameter angehängt.');
 
-$field = $form->addCheckboxField('check_weight');
-$field->setLabel('Artikelgewicht prüfen');
-$field->addOption('Artikelgewicht prüfen', "1");
-$field->setNotice('Wenn die Checkbox angewählt ist, wird bei der Artikeleingabe im Backend geprüft, ob auch ein Gewicht angegeben wurde. Es ist dann nicht möglich, Artikel ohne Gewicht zu erfassen. Für die Gewichtsprüfung muss zusätzlich in der Artikeltabelle in yform die Customfunction warehouse::check_input_weight zur Validierung verwendet werden.<br><code>rex_config::get("warehouse","check_weight")</code>');
+// TODO: Gewicht ausblenden / einblenden in Formularen und in Optionen berücksichtigen / nicht berücksichtigen
 
+$field = $form->addCheckboxField('enable_weight');
+$field->setLabel('Artikelgewicht verwenden');
+$field->addOption('Artikelgewicht verwenden', "1");
+$field->setAttribute('disabled', 'disabled');
 
 $field = $form->addInputField('text', 'editor', null, ['class' => 'form-control']);
-$field->setLabel(rex_i18n::msg('warehouse_editor'));
-$field->setNotice('z.B. <code>class="form-control redactor-editor--default"</code>');
+$field->setLabel(rex_i18n::msg('warehouse.settings.editor'));
+$field->setNotice(rex_i18n::msg('warehouse.settings.editor.notice'));
 
 
 $content = $form->get();
 
 $fragment = new rex_fragment();
-$fragment->setVar('title', 'Einstellungen');
+$fragment->setVar('title', rex_i18n::msg('warehouse_settings_general'));
 $fragment->setVar('body', $content, false);
 $content = $fragment->parse('core/page/section.php');
 
