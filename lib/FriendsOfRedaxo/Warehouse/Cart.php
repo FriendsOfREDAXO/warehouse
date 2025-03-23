9285ec8663c21360f9411aec900b411567a8443b
Alexander Walther <mail@alexplus.de>
2025-03-23 15:50:54 +0100

    Update Cart.php

--- lib/FriendsOfRedaxo/Warehouse/Cart.php
+++ lib/FriendsOfRedaxo/Warehouse/Cart.php
@@ -4,7 +4,15 @@ namespace FriendsOfRedaxo\Warehouse;
 
 class Cart
 {
-    public static function countPieces()
+    public $cart = [];
+    // initialisieren des Warenkorbs
+    public static function init()
+    {
+        if (rex_session('warehouse_cart', 'array', null) === null) {
+            rex_set_session('warehouse_cart', []);
+        }
+    }
+    public static function countItems()
     {
         $cart = Warehouse::getCart();
         $sum_pcs = 0;
