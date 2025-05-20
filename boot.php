1bfaf0759ad19bd5962891a9bd6a64dbfbd1597f
Alexander Walther <aw@alexplus.de>
2025-05-20 13:54:04 +0200

    in boot.php EPs registrieren, Wallee entfernen und Abhängigkeiten vom URL-Addon entfernen

--- boot.php
+++ boot.php
@@ -42,77 +42,39 @@ if (rex::isFrontend()) {
 
     rex_extension::register('PACKAGES_INCLUDED', function () {
 
-        if (rex_request('action', 'string') == 'add_to_cart') {
-            // Warehouse::addToCart();
-        }
-        if (rex_request('action', 'string') == 'modify_cart') {
-            // Warehouse::modifyCart();
-        }
+        if (rex_addon::get('url') !== null) {
+            $manager = Url::resolveCurrent();
+            if ($manager) {
+                $profile = $manager->getProfile();
+                $seo = $manager->getSeo();
 
-        if (rex_article::getCurrentId() == Warehouse::getConfig('order_page')) {
-            Warehouse::emptyCart();
-        }
-
-        $manager = Url::resolveCurrent();
-        if ($manager) {
-            $profile = $manager->getProfile();
-            $seo = $manager->getSeo();
-
-            $data_id = (int) $manager->getDatasetId();
-            if ($profile->getTableName() == rex::getTable('warehouse_article')) {
-                if ($var_id = rex_get('var_id', 'int')) {
-                    // $article = Article::getArt(0, [$data_id, $var_id], true);
-                } else {
-                    // $article = Article::get_articles(0, [$data_id], true, 0, 1);
+                $data_id = (int) $manager->getDatasetId();
+                if ($profile->getTableName() == rex::getTable('warehouse_article')) {
+                    $warehouse_prop['sitemode'] = 'article';
+                } elseif ($profile->getTableName() == rex::getTable('warehouse_category')) {
+                    $warehouse_prop['sitemode'] = 'category';
+                    $warehouse_prop['seo_title'] = $seo['title'];
+                    $warehouse_prop['path'] = Warehouse::getCategoryPath($data_id);
                 }
-                $warehouse_prop['sitemode'] = 'article';
-                // $warehouse_prop['seo_title'] = $article->getName();
-                // $warehouse_prop['path'] = Warehouse::getCategoryPath($article->getCategoryId());
-            } elseif ($profile->getTableName() == rex::getTable('warehouse_category')) {
-                $warehouse_prop['sitemode'] = 'category';
-                $warehouse_prop['seo_title'] = $seo['title'];
-                $warehouse_prop['path'] = Warehouse::getCategoryPath($data_id);
-            }
-            $curl = rtrim(rex_yrewrite::getFullPath(), '/') . $_SERVER['REQUEST_URI'];
-            rex_set_session('current_page', $curl);
-        }
-        // $warehouse_prop['tree'] = Warehouse::get_category_tree();
-        rex::setProperty('warehouse_prop', $warehouse_prop);
-
-        if (rex_article::getCurrentId() == Warehouse::getConfig('thankyou_page')) {
-            // Bei Dankeseite Paypal bestätigen
-            if (rex_get('paymentId')) {
-                // Warehouse::set_cart_from_payment_id(rex_get('paymentId'));
-                PayPal::ExecutePayment();
-                // Führt den E-Mail Versand im Hintergrund aus
-                Warehouse::emptyCart();
+                $curl = rtrim(rex_yrewrite::getFullPath(), '/') . $_SERVER['REQUEST_URI'];
+                rex_set_session('current_page', $curl);
             }
 
+            rex::setProperty('warehouse_prop', $warehouse_prop);
 
-            // Bei Dankeseite Wallee - Zahlungsbestätigung Wallee
-            if (rex_get('action') == 'wpayment_confirm') {
-                $user_data = Warehouse::getCustomerData();
-                if (rex_get('key') == $user_data['payment_confirm']) {
-                    // Confirm Order in db
-                    $order = Order::query()
-                        ->where('payment_confirm', rex_get('key'))->where('payed', 0)
-                        ->findOne();
-                    if (!$order) {
-                        rex_redirect(rex_config::get("warehouse", "payment_error"));
-                    }
-                    $order->setValue('payed', 1);
-                    if (!$order->save()) {
-                        rex_redirect(Warehouse::getConfig("payment_error"));
-                    }
-
-                    // Send Mails
-                    // Warehouse::send_mails();
-                    // Warehouse::updateStockAfterOrder();
+            if (rex_article::getCurrentId() == Warehouse::getConfig('thankyou_page')) {
+                if (rex_get('paymentId')) {
+                    PayPal::ExecutePayment();
                     Warehouse::emptyCart();
-                } else {
-                    rex_redirect(Warehouse::getConfig("payment_error"));
                 }
             }
         }
     });
+}
+
+if (rex::isBackend()) {
+    rex_extension::register('YFORM_DATA_LIST', Article::epYformDataList(...));
+    rex_extension::register('YFORM_DATA_LIST', Category::epYformDataList(...));
+    rex_extension::register('YFORM_DATA_LIST', Order::epYformDataList(...));
+    rex_extension::register('YFORM_DATA_LIST_ACTION_BUTTONS',Order::epYformDataListActionButtons(...));
 }
