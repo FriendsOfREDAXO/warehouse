411735441a054382f99a17de5ecbb134892d6ed7
Alexander Walther <aw@alexplus.de>
2025-05-25 11:42:06 +0200

    Update order.details.php

--- pages/order.details.php
+++ pages/order.details.php
@@ -10,6 +10,11 @@ echo rex_view::title($addon->i18n('warehouse.title'));
 
 $table_name = 'rex_warehouse_order';
 
+if (rex_request('data_id', 'int') <= 0) {
+    echo rex_view::error($addon->i18n('warehouse.order.details.error.no_data_id'));
+    return;
+}
+
 $order = FriendsOfRedaxo\Warehouse\Order::get(rex_request('data_id', 'int'));
 
 // Ein schönes Bootstrap 3 Layout eines Lieferscheins
@@ -43,34 +48,39 @@ $order = FriendsOfRedaxo\Warehouse\Order::get(rex_request('data_id', 'int'));
 
 ?>
 <div class="panel">
-    <div class="container content">
-        <div class="row">
-            <div class="col-md-3">
-                <?= $order->getSalutation() ?> <?= $order->getFirstname() ?> <?= $order->getLastname() ?><br />
-                <?= $order->getCompany() ?><br />
-                <?= $order->getAddress() ?> <?= $order->getZip() ?> <?= $order->getCity() ?><br />
-                <?= $order->getCountry() ?><br />
-                <?= $order->getEmail() ?><br />
-            </div>
-            <div class="col-md-6"></div>
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-header">
-                        <span class="badge">
-                            <?= $order->getPaymentType() ?>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Bestelldetails zu Bestell-ID: <?= $order->getId() ?>
-            </div>
-        </div>
-        <div class="row cart">
-            <div class="col-md-12">
-            </div>
-        </div>
-    </div>
+	<div class="container content">
+		<div class="row">
+			<div class="col-md-3">
+				<?= $order->getSalutation() ?>
+				<?= $order->getFirstname() ?>
+				<?= $order->getLastname() ?><br />
+				<?= $order->getCompany() ?><br />
+				<?= $order->getAddress() ?>
+				<?= $order->getZip() ?>
+				<?= $order->getCity() ?><br />
+				<?= $order->getCountry() ?><br />
+				<?= $order->getEmail() ?><br />
+			</div>
+			<div class="col-md-6"></div>
+			<div class="col-md-3">
+				<div class="card">
+					<div class="card-header">
+						<span class="badge">
+							<?= $order->getPaymentType() ?>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Bestelldetails zu Bestell-ID:
+					<?= $order->getId() ?>
+			</div>
+		</div>
+		<div class="row cart">
+			<div class="col-md-12">
+			</div>
+		</div>
+	</div>
 </div>
