ebd4851e8ace367731903d2fab8da8049d9dab2e
Alexander Walther <aw@alexplus.de>
2025-05-25 18:31:34 +0200

    Update url_profile.php

--- install/url_profile.php
+++ install/url_profile.php
@@ -7,17 +7,76 @@
  */
 $sql = rex_sql::factory();
 $sql->setTable(rex::getTable('url_generator_profile'));
-$sql->setValue('namespace', 'category_id');
+$sql->setValue('namespace', 'warehouse-category-id');
 $sql->setValue('article_id', rex_article::getSiteStartArticleId());
-$sql->setValue('clang_id', 1);
+$sql->setValue('clang_id', rex_clang::getCurrentId());
 $sql->setValue('ep_pre_save_called', 0);
 $sql->setValue('table_name', '1_xxx_rex_warehouse_category');
 $sql->setValue('table_parameters', json_encode([
     'column_id' => 'id',
     'column_clang_id' => '',
-    'restriction_1_column' => '',
-    'restriction_1_comparison_operator' => '=',
-    'restriction_1_value' => '',
+    'restriction_1_column' => 'status',
+    'restriction_1_comparison_operator' => '>=',
+    'restriction_1_value' => '0',
+    'restriction_2_logical_operator' => 'AND',
+    'restriction_2_column' => 'parent_id',
+    'restriction_2_comparison_operator' => '= ""',
+    'restriction_2_value' => '',
+    'restriction_3_logical_operator' => '',
+    'restriction_3_column' => '',
+    'restriction_3_comparison_operator' => '=',
+    'restriction_3_value' => '',
+    'column_segment_part_1' => 'name',
+    'column_segment_part_2_separator' => '/',
+    'column_segment_part_2' => '',
+    'column_segment_part_3_separator' => '/',
+    'column_segment_part_3' => '',
+    'relation_1_column' => 'parent_id',
+    'relation_1_position' => 'BEFORE',
+    'relation_2_column' => '',
+    'relation_2_position' => 'BEFORE',
+    'relation_3_column' => '',
+    'relation_3_position' => 'BEFORE',
+    'append_user_paths' => '',
+    'append_structure_categories' => '0',
+    'column_seo_title' => 'name',
+    'column_seo_description' => 'teaser',
+    'column_seo_image' => 'image',
+    'sitemap_add' => '1',
+    'sitemap_frequency' => 'always',
+    'sitemap_priority' => '1.0',
+    'column_sitemap_lastmod' => 'updatedate',
+]));
+$sql->setValue('relation_1_table_name', 'relation_1_xxx_1_xxx_rex_warehouse_category');
+$sql->setValue('relation_1_table_parameters', json_encode([
+    'column_id' => 'id',
+    'column_clang_id' => '',
+    'column_segment_part_1' => 'name',
+    'column_segment_part_2_separator' => '/',
+    'column_segment_part_2' => '',
+    'column_segment_part_3_separator' => '/',
+    'column_segment_part_3' => '',
+]));
+$sql->setValue('relation_2_table_name', '');
+$sql->setValue('relation_2_table_parameters', '[]');
+$sql->setValue('relation_3_table_name', '');
+$sql->setValue('relation_3_table_parameters', '[]');
+$sql->addGlobalCreateFields('warehouse');
+$sql->addGlobalUpdateFields('warehouse');
+$sql->insert();
+
+$sql->setTable(rex::getTable('url_generator_profile'));
+$sql->setValue('namespace', 'warehouse-article-id');
+$sql->setValue('article_id', rex_article::getSiteStartArticleId());
+$sql->setValue('clang_id', rex_clang::getCurrentId());
+$sql->setValue('ep_pre_save_called', 0);
+$sql->setValue('table_name', '1_xxx_rex_warehouse_article');
+$sql->setValue('table_parameters', json_encode([
+    'column_id' => 'id',
+    'column_clang_id' => '',
+    'restriction_1_column' => 'status',
+    'restriction_1_comparison_operator' => '>=',
+    'restriction_1_value' => '0',
     'restriction_2_logical_operator' => '',
     'restriction_2_column' => '',
     'restriction_2_comparison_operator' => '=',
@@ -26,9 +85,9 @@ $sql->setValue('table_parameters', json_encode([
     'restriction_3_column' => '',
     'restriction_3_comparison_operator' => '=',
     'restriction_3_value' => '',
-    'column_segment_part_1' => 'name_1',
+    'column_segment_part_1' => 'name',
     'column_segment_part_2_separator' => '/',
-    'column_segment_part_2' => '',
+    'column_segment_part_2' => 'id',
     'column_segment_part_3_separator' => '/',
     'column_segment_part_3' => '',
     'relation_1_column' => '',
@@ -39,9 +98,9 @@ $sql->setValue('table_parameters', json_encode([
     'relation_3_position' => 'BEFORE',
     'append_user_paths' => '',
     'append_structure_categories' => '0',
-    'column_seo_title' => '',
-    'column_seo_description' => '',
-    'column_seo_image' => '',
+    'column_seo_title' => 'name',
+    'column_seo_description' => 'short_text',
+    'column_seo_image' => 'image',
     'sitemap_add' => '1',
     'sitemap_frequency' => 'always',
     'sitemap_priority' => '1.0',
@@ -58,11 +117,11 @@ $sql->addGlobalUpdateFields('warehouse');
 $sql->insert();
 
 $sql->setTable(rex::getTable('url_generator_profile'));
-$sql->setValue('namespace', 'warehouse-article-id');
+$sql->setValue('namespace', 'warhouse-order-id');
 $sql->setValue('article_id', rex_article::getSiteStartArticleId());
-$sql->setValue('clang_id', 1);
+$sql->setValue('clang_id', rex_clang::getCurrentId());
 $sql->setValue('ep_pre_save_called', 0);
-$sql->setValue('table_name', '1_xxx_rex_warehouse_article');
+$sql->setValue('table_name', '1_xxx_rex_warehouse_order');
 $sql->setValue('table_parameters', json_encode([
     'column_id' => 'id',
     'column_clang_id' => '',
@@ -77,9 +136,9 @@ $sql->setValue('table_parameters', json_encode([
     'restriction_3_column' => '',
     'restriction_3_comparison_operator' => '=',
     'restriction_3_value' => '',
-    'column_segment_part_1' => 'name_1',
+    'column_segment_part_1' => 'id',
     'column_segment_part_2_separator' => '/',
-    'column_segment_part_2' => 'id',
+    'column_segment_part_2' => '',
     'column_segment_part_3_separator' => '/',
     'column_segment_part_3' => '',
     'relation_1_column' => '',
@@ -90,13 +149,13 @@ $sql->setValue('table_parameters', json_encode([
     'relation_3_position' => 'BEFORE',
     'append_user_paths' => '',
     'append_structure_categories' => '0',
-    'column_seo_title' => 'name_1',
-    'column_seo_description' => 'description_1',
-    'column_seo_image' => 'image',
-    'sitemap_add' => '1',
-    'sitemap_frequency' => 'always',
-    'sitemap_priority' => '1.0',
-    'column_sitemap_lastmod' => 'updatedate',
+    'column_seo_title' => '',
+    'column_seo_description' => '',
+    'column_seo_image' => '',
+    'sitemap_add' => '0',
+    'sitemap_frequency' => 'never',
+    'sitemap_priority' => '0.0',
+    'column_sitemap_lastmod' => '',
 ]));
 $sql->setValue('relation_1_table_name', '');
 $sql->setValue('relation_1_table_parameters', '[]');
