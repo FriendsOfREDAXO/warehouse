01cde5bd448c5183bc0f93375d8fc68b5a153e65
Alexander Walther <aw@alexplus.de>
2025-05-25 18:18:25 +0200

    Neue Backend-Pages

---
+++ pages/order.customer_address.php
@@ -0,0 +1,33 @@
+<?php
+use FriendsOfRedaxo\Warehouse\Search;
+
+/**
+ * @var rex_addon $this
+ * @psalm-scope-this rex_addon
+ */
+
+$addon = rex_addon::get('warehouse');
+echo rex_view::title($addon->i18n('warehouse.title'));
+
+$table_name = 'rex_warehouse_customer_address';
+
+rex_extension::register(
+    'YFORM_MANAGER_DATA_PAGE_HEADER',
+    static function (rex_extension_point $ep) {
+        if ($ep->getParam('yform')->table->getTableName() === $ep->getParam('table_name')) {
+            return '';
+        }
+    },
+    rex_extension::EARLY,
+    ['table_name' => $table_name],
+);
+
+// @phpstan-ignore-next-line
+$_REQUEST['table_name'] = $table_name;
+?>
+
+<div class="rex-page-section">
+    <?= Search::getForm() ?>
+</div>
+<?php
+include rex_path::plugin('yform', 'manager', 'pages/data_edit.php');
