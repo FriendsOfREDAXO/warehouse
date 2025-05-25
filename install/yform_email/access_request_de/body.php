6a37e55ac4d3bfdf14402ae7b035deb70d1c32ce
Alexander Walther <aw@alexplus.de>
2025-05-25 16:30:53 +0200

    remove access_request_de yform_email template

--- install/yform_email/access_request_de/body.php
+++
@@ -1,3 +0,0 @@
-Bitte klicken Sie diesen Link, um die Anmeldung zu bestätigen:
-
-<?= trim(rex::getServer(),'/') . rex_getUrl(22) ?>?rex_ycom_activation_key=REX_YFORM_DATA[field=activation_key]&rex_ycom_id=REX_YFORM_DATA[field=email]
