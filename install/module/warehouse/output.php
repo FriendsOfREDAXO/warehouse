2ba8f4ec1a5fa396a4ee0e2218511970f98df229
Alexander Walther <aw@alexplus.de>
2025-05-25 11:41:57 +0200

    Modul aktualisiert

--- install/module/warehouse/output.php
+++ install/module/warehouse/output.php
@@ -1,12 +1,17 @@
 <?php
 /* Dieses Modul wird automatisch vom Addon `warehouse` aktualisiert. Es wird empfohlen, keine Änderungen vorzunehmen, da diese nicht updatesicher sind. */
 
-$file = "REX_VALUE[10]" ? "REX_VALUE[10]" : "fragments/warehouse/index.php";
+use FriendsOfRedaxo\Warehouse\Warehouse;
+
+$title = "REX_VALUE[1]";
+$description = "REX_VALUE[2 output=html]";
+
+/* Passendes Fragment ausgeben, Parameter des Dateinamens in REX_VALUE[10] */
+$file = "REX_VALUE[10]" ? "REX_VALUE[10]" : "index.php";
 
 if (rex_addon::get('warehouse')->isAvailable()) {
-    $fragment = new rex_fragment();
-    echo $fragment->parse($file);
+    echo Warehouse::parse($file, ['title' => $title, 'description' => $description]);
     return;
 } else {
-    echo rex_view::error('Das Addon "Warehouse" ist nicht installiert und aktiviert.');
+    echo rex_view::error('Das Add-on "Warehouse" ist nicht installiert oder aktiviert.');
 }
