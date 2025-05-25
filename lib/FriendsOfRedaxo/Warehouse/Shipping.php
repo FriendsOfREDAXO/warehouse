7af511961d20db1cc1b173015cfb21154f400a71
Alexander Walther <aw@alexplus.de>
2025-05-25 16:32:08 +0200

    Update Shipping.php

--- lib/FriendsOfRedaxo/Warehouse/Shipping.php
+++ lib/FriendsOfRedaxo/Warehouse/Shipping.php
@@ -17,11 +17,11 @@ class Shipping {
 
     public static function getCost() {
 
-        $cart = Warehouse::getCart();
+        $cart = Cart::get();
 
-        $total_weight = Cart::weighWeight();
-        $total_pieces = Cart::countItems();
-        $total_price = Cart::calculateTotal();
+        $total_weight = $cart->totalWeight();
+        $total_pieces = $cart->count();
+        $total_price = $cart->getTotal();
 
         $free_shipping_from = (float) rex_config::get('warehouse', 'free_shipping_from');
         $shipping_fee = (float) rex_config::get('warehouse', 'shipping_fee');
@@ -29,7 +29,7 @@ class Shipping {
         $shipping_calculation_mode = (string) rex_config::get('warehouse', 'shipping_calculation_mode');
 
         $return = $shipping_fee;
-        // Wenn Standard-Shipping-MOdus ausgewählt ist, dann nur Standard-Berechnung durchführen
+        // Wenn Standard-Shipping-Modus ausgewählt ist, dann nur Standard-Berechnung durchführen
         if($shipping_calculation_mode == '') {
             if ($total_price >= $free_shipping_from) {
                 $return = 0;
