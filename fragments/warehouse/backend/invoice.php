b6f2fa446167238c251f4ec463206e02ec5862d9
Alexander Walther <aw@alexplus.de>
2025-06-03 01:10:26 +0200

    add vendor - fontawesome 6.7.x free

--- fragments/warehouse/backend/invoice.php
+++ fragments/warehouse/backend/invoice.php
@@ -45,7 +45,7 @@ if (empty($userNumber)) {
 <html>
 
 <head>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= rex_path::addonAssets('warehouse', 'vendor\fontawesome-free\webfonts\fa-regular-400.ttf'); ?>">
 	<style>
 		@page {
 			margin: 0cm 0cm;
