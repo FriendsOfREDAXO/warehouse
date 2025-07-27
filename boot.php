59efc1b82528c78a21998ea62514509a26098514
Alexander Walther <aw@alexplus.de>
2025-07-27 18:54:33 +0200

    YCom User Model Class als Instanz von "Customer" laden

--- boot.php
+++ boot.php
@@ -32,6 +32,12 @@ if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_dataset::setModelClass('rex_ycom_user', Customer::class);
     rex_yform_manager_dataset::setModelClass('rex_warehouse_customer_address', CustomerAddress::class);
 }
+rex_extension::register('PACKAGES_INCLUDED', function () {
+    if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
+        rex_yform_manager_dataset::setModelClass('rex_ycom_user', Customer::class);
+    }
+});
+
 
 rex_yform::addTemplatePath($this->getPath('ytemplates'));
 
