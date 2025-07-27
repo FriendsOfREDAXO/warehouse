b3874b2422087a84eb6bd644e211cb98786be6a8
Alexander Walther <aw@alexplus.de>
2025-07-27 19:10:12 +0200

    Im Checkout Customer aus Session laden und in Session speichern können

--- lib/FriendsOfRedaxo/Warehouse/Checkout.php
+++ lib/FriendsOfRedaxo/Warehouse/Checkout.php
@@ -2,22 +2,25 @@
 
 namespace FriendsOfRedaxo\Warehouse;
 
+use Address;
 use rex_yform;
 
-class Checkout {
+class Checkout
+{
 
-    public static function getContinueAsGuestForm() : \rex_yform
+    public static function getContinueAsGuestForm(): \rex_yform
     {
         // Formular für Gast-Bestellung
         // Hier wird ein YForm-Objekt erstellt, das die Gast-Bestellung ermöglicht       
         $yform = new rex_yform();
-                
+
         $yform->setObjectparams('form_action', Domain::getCurrent()->getCheckoutUrl());
         $yform->setObjectparams('form_wrap_class', 'warehouse_checkout_guest');
         $yform->setObjectparams('form_ytemplate', 'bootstrap5,bootstrap');
         $yform->setObjectparams('form_class', 'rex-yform warehouse_checkout_guest');
         $yform->setObjectparams('form_anchor', '');
         $yform->setObjectparams('form_name', 'warehouse_checkout_guest');
+        $yform->setObjectparams('real_field_names', true);
 
         // Ausschließlich einen Submit-Button für die Gast-Bestellung
         $yform->setValueField('submit_once', [
@@ -35,23 +38,24 @@ class Checkout {
         return $yform;
     }
 
-    public static function getLoginForm() : rex_yform
+    public static function getLoginForm(): rex_yform
     {
         // Formular für Login
         $yform = new rex_yform();
-        
+
         $yform->setObjectparams('form_action', Domain::getCurrent()->getCheckoutUrl());
         $yform->setObjectparams('form_wrap_class', 'warehouse_checkout_login');
         $yform->setObjectparams('form_ytemplate', 'bootstrap5,bootstrap');
         $yform->setObjectparams('form_class', 'rex-yform warehouse_checkout_login');
         $yform->setObjectparams('form_anchor', '');
         $yform->setObjectparams('form_name', 'warehouse_checkout_login');
+        $yform->setObjectparams('real_field_names', true);
 
         // YCom Auth Validierung für Login
         $yform->setValidateField('ycom_auth', [
-            'login', 
-            'password', 
-            null, 
+            'login',
+            'password',
+            null,
             'Bitte geben Sie Benutzername und Passwort ein',
             'Login fehlgeschlagen. Bitte überprüfen Sie Ihre Eingaben.'
         ]);
@@ -87,5 +91,64 @@ class Checkout {
         ]);
 
         return $yform;
+    }
+
+    /**
+     * Nachdem das Gäste-Formular abgesendet wurde, werden die 
+     * Angaben des Kunden in der Session zwischengespeichert, 
+     * um sie beim Bezahlvorgang verfügbar zu haben.
+     * @param type $params
+     */
+    public static function saveCustomerInSession($params)
+    {
+        $value_pool = $params->params['value_pool']['email'];
+        $customer_session = [];
+        foreach ($value_pool as $field => $value) {
+            // Wenn es ein Feld mit 'to_' startet
+            if (str_starts_with($field, 'to_')) {
+                // Entferne 'to_' vom Feldnamen
+                $new_field = substr($field, 3);
+                // Speichere den Wert im Session-Array
+                $customer_session[$new_field] = $value;
+            } else {
+                // Andernfalls speichere den Wert direkt
+                $customer_session[$field] = $value;
+            }
+        }
+
+        rex_set_session('user_data', $customer_session);
+    }
+
+    public static function loadCustomerFromSession(): ?Customer
+    {
+        $user_data = rex_session('user_data', 'array', []);
+        if (empty($user_data)) {
+            return null;
+        }
+
+        // Erstelle ein Customer-Objekt aus den Session-Daten
+        $customer = Customer::create();
+        $customer->setFirstname($user_data['firstname'] ?? '');
+        $customer->setLastname($user_data['lastname'] ?? '');
+        $customer->setCompany($user_data['company'] ?? '');
+        $customer->setDepartment($user_data['department'] ?? '');
+        $customer->setAddress($user_data['address'] ?? '');
+        $customer->setZip($user_data['zip'] ?? '');
+        $customer->setCity($user_data['city'] ?? '');
+        $customer->setEmail($user_data['email'] ?? '');
+        $customer->setPhone($user_data['phone'] ?? '');
+
+        // Setze die Lieferadresse, falls vorhanden
+        if (isset($user_data['to_name'])) {
+            $shippingAddress = CustomerAddress::create();
+            $shippingAddress->setName($user_data['to_name']);
+            $shippingAddress->setCompany($user_data['to_company'] ?? '');
+            $shippingAddress->setStreet($user_data['to_address'] ?? '');
+            $shippingAddress->setZip($user_data['to_zip'] ?? '');
+            $shippingAddress->setCity($user_data['to_city'] ?? '');
+            // $customer->setShippingAddress($shippingAddress);
+        }
+
+        return $customer;
     }
 }
